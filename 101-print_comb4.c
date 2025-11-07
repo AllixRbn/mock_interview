41cdb6903b5f043ee06a6bb9f773246adc416292
Allix <thewitchonxanax@gmail.com>
2025-11-07 10:08:18 +0000

    Fix typo in documentation and restructure main function for printing combinations of three digits

--- 101-print_comb4.c
+++ 101-print_comb4.c
@@ -2,9 +2,34 @@
 
 /**
 *
-*rints all possible different combinations of three digits.
+*prints all possible different combinations of three digits.
 *
 *Return : always 0
 */
 
 int main()
+{
+    int a, b, c;
+
+    for (a = 0; a <= 7; a++)
+    {
+        for (b = 0; b <= 8; b++)
+        {
+            for (c = 0; c <= 9; c++)
+            {
+                putchar (a + '0');
+                putchar (b + '0');
+                putchar (c + '0');
+
+                if (a != 7 | b != 8 | c != 9)
+                {
+                    putchar (',');
+                    putchar (' ');
+                }
+
+            }
+        }
+    }
+    putchar ('\n');
+    return (0);
+}
